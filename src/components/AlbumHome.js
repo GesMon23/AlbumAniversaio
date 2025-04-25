ee1a7a0f0eb424fa9279577f6ebd1f33b6035eb1
GesMon23 <100744918+GesMon23@users.noreply.github.com>
2025-04-24 19:48:17 -0600

    Pub finalll

--- src/components/AlbumHome.js
+++ src/components/AlbumHome.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './AlbumHome.css';
+import './AlbumHome.mobile.rows.css';
 import BlogPost from './BlogPost';
 import WideBlock from './WideBlock';
 import GalleryRow from './GalleryRow';
